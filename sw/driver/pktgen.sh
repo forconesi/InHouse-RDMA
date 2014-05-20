3ce762efb404653d0297c1dfe59d188a29685bc5
Hwanju Kim <hwandori@gmail.com>
2014-05-20 17:01:00 +0100

    nf10-newnic: pktgen.sh accepts count as an env var

--- sw/driver/pktgen.sh
+++ sw/driver/pktgen.sh
@@ -47,8 +47,9 @@ if [ ! -e $PGDEV ]; then
 	echo "Error: pktgen might not be loaded or $netif is not up now!"
 	exit
 fi
-#count=1000000
-count=30000
+if [ "$count" = "" ]; then
+	count=1000000
+fi
 pgset "count $count"
 pgset "clone_skb 100000"
 pgset "pkt_size $pkt_size"	# nic adds 4B CRC
